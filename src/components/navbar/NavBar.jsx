08956919d6ba858f16d33458d409e9de23930e40
Bifabig <biftubig@gmail.com>
2024-02-19 15:31:08 +0300

    logo

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -4,7 +4,7 @@ import styles from './navbar.module.css';
 const NavBar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Logo</div>
+      <div className={styles.logo}>MyLogo</div>
       <div>
         <Links />
       </div>
